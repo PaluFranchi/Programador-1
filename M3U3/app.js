55ac3ebe5fc0beae10c5825a3815b5bd5adb3302
Paloma Franchimont <palomafranchimont@gmail.com>
2022-04-21 20:09:05 -0300

    formulario de contacto

--- M3U3/app.js
+++ M3U3/app.js
@@ -4,8 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var recetasRouter = require('./routes/recetas');
+var quienessomosRouter = require('./routes/quienessomos');
+var contactoRouter = require('./routes/contacto');
 
 var app = express();
 
@@ -21,6 +26,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/recetas', recetasRouter);
+app.use('/quienessomos', quienessomosRouter);
+app.use('/contacto', contactoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
