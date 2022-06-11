ec932b329c3c82cf347eaf11f82bd9604587871a
Paloma Franchimont <palomafranchimont@gmail.com>
2022-06-11 19:07:40 -0300

    Agregado de imagenes

--- M5U3/app.js
+++ M5U3/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 require('dotenv').config();
 var session = require('express-session');
+var fileUpload = require('express-fileupload');
 
 var pool = require('./models/bd');
 
@@ -48,6 +49,11 @@ secured = async (req, res, next) => {
     console.log(error);
   }
 };
+
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: '/tmp/'
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- M5U3/routes/admin/novedades.js
+++ M5U3/routes/admin/novedades.js
@@ -2,12 +2,36 @@ var express = require('express');
 const pool = require('../../models/bd');
 var router = express.Router();
 var novedadesModel = require('../../models/novedadesModel');
+var util = require('util');
+const { raw } = require('mysql');
+var cloudinary = require('cloudinary').v2;
+
+var uploader = util.promisify(cloudinary.uploader.upload);
+const destroy = util.promisify(cloudinary.uploader.destroy);
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
 
   var novedades = await novedadesModel.getNovedades();
 
+  novedades = novedades.map(novedad => {
+    if (novedad.img_id) {
+      const imagen = cloudinary.image(novedad.img_id, {
+        width: 80,
+        height: 80,
+        crop: 'fill'
+      });
+      return {
+        ...novedad,
+        imagen
+      }
+    } else {
+      return {
+        ...novedad,
+        imagen: ' '
+      }
+    }
+  });
   res.render('admin/novedades', {
     layout: 'admin/layout',
     usuario: req.session.nombre,
@@ -15,16 +39,22 @@ router.get('/', async function (req, res, next) {
   });
 });
 
+
 // Para eliminar una novedad
 router.get('/eliminar/:id', async (req, res, next) => {
   var id = req.params.id;
+
+  let novedad = await novedadesModel.getNovedadesById(id);
+  if (novedad.img_id) {
+    await (destroy(novedad.img_id));
+  }
 
   await novedadesModel.deleteNovedadesById(id);
   res.redirect('/admin/novedades')
 });
 // cierra get de eliminar
 
-// formulario para agregar novedades 
+// formulario para agregar novedades (render)
 
 router.get('/agregar', (req, res, next) => {
   res.render('admin/agregar', {
@@ -32,11 +62,21 @@ router.get('/agregar', (req, res, next) => {
   })
 });
 // cierra get agregar 
-// agregar cuando toco el boton guardar
+// agregar cuando toco el boton guardar (procesador)
 router.post('/agregar', async (req, res, next) => {
   try {
+
+    var img_id = '';
+    if (req.files && Object.keys(req.files).length > 0) {
+      imagen = req.files.imagen;
+      img_id = (await uploader(imagen.tempFilePath)).public_id;
+    }
+
     if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
-      await novedadesModel.insertNovedad(req.body);
+      await novedadesModel.insertNovedad({
+        ...req.body,
+        img_id
+      });
       res.redirect('/admin/novedades')
     } else {
       res.render('admin/agregar', {
@@ -69,10 +109,28 @@ router.get('/modificar/:id', async (req, res, next) => {
 //modificar post
 router.post('/modificar', async (req, res, next) => {
   try {
+    
+    let img_id = req.body.img_original;
+    let borrar_img_vieja = false;
+    if (req.body.img_delete === "1") {
+      img_id = null;
+      borrar_img_vieja = true;
+    } else {
+      if (req.files && Object.keys(req.files).length > 0) {
+        imagen = req.files.imagen;
+        img_id = (await uploader(imagen.tempFilePath)).public_id;
+        borrar_img_vieja = true;
+      }
+    }
+    if (borrar_img_vieja && req.body.img_original) {
+      await (destroy(req.body.img_original));
+    }
+
     var obj = {
       titulo: req.body.titulo,
       subtitulo: req.body.subtitulo,
-      cuerpo: req.body.cuerpo
+      cuerpo: req.body.cuerpo,
+      img_id
     }
 
     await novedadesModel.modificarNovedadesById(obj, req.body.id);
@@ -81,7 +139,7 @@ router.post('/modificar', async (req, res, next) => {
     console.log(error)
     res.render('admin/modificar', {
       layout: 'admin/layout',
-      error: true, 
+      error: true,
       message: 'No se modificó la novedad'
     })
   }
